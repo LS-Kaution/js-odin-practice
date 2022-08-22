fabf19a717688ee30f2b2b6075bcd0d19166a0b2
LS-Kaution <leonardosaturnoplaneta@gmail.com>
2022-08-22 11:16:18 -0500

    add fundamentals-2

---
+++ fundamentals-2/Exercise-3/math.js
@@ -0,0 +1,20 @@
+/**
+ * Lets do some math!
+ * Some rules first:
+ *   Enter the operations, replacing the `"?"`, make the computer do the work for you. 
+ *   Do not manually enter the answer, for example: "one plus ten" would look like 1+10
+ * 
+ * a = one plus eight
+ * b = 22 times three
+ * c = the *remainder* of 5/4
+ * d = the variable 'a' minus 17
+ * e = the sum of the previous four variables
+ */
+
+const a = 9
+const b = 66
+const c = 1
+const d = -8
+const e = 68
+
+export { a, b, c, d, e }
