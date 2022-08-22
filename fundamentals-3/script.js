49af47eece6b18921914a23b171d642177c3c585
LS-Kaution <leonardosaturnoplaneta@gmail.com>
2022-08-22 15:37:03 -0500

    add fundamentals 3

---
+++ fundamentals-3/script.js
@@ -0,0 +1,32 @@
+function add7(number){
+    let resolved = number + 7
+    return resolved
+}
+
+console.log(add7(23))
+
+function multiply(a, b){
+    let resolved = a * b
+    return resolved
+}
+
+console.log(multiply(14, 2))
+
+function capitalize (string){
+    return string.charAt(0).toUpperCase()
+}
+
+console.log(capitalize('marico si'))
+
+function lastLetter (string){
+    return string.charAt(8)
+}
+
+console.log(lastLetter('marico si'))
+
+
+
+
+
+
+
